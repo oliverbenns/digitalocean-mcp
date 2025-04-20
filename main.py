24ed281d1fa4965a2987c69ee94afd2f57a2f79c
Oliver Benns <oliver@benns.co>
2025-04-20 10:46:49 +0100

    Add get apps tool

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from digitalocean-mcp!")
+from typing import Any
+import os
+from mcp.server.fastmcp import FastMCP
+from pydo import Client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+access_token = os.getenv("DIGITALOCEAN_ACCESS_TOKEN")
+
+client = Client(token=access_token)
+
+get_resp = client.apps.list()
+
+mcp = FastMCP("digitalocean")
+
+@mcp.tool()
+def get_apps(state: str) -> str:
+    """Get all digital ocean apps.
+    """
+    data = client.apps.list()
+
+    return data
 
 
 if __name__ == "__main__":
-    main()
+    print("Starting digital ocean server...")
+
+    mcp.run(transport='stdio')
